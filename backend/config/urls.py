16cb31c8c64e0bc39f54587996a71a53740434b6
AKHIL A <msdcr88@gmail.com>
2026-03-17 14:18:21 +0530

    deploy on the way

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return HttpResponse("OK")
+
 urlpatterns = [
+    path("", home), 
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
